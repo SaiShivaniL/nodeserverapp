7232ba975d547f26f0edd922ed708435fd95af33
SaiShivaniL <saishivani.wnp@gmail.com>
2022-09-07 10:23:05 +0530

    save to github

---
+++ index.js
@@ -0,0 +1,9 @@
+var express=require('express')
+var app=express()
+app.get("/",(req,res)=>{
+    res.send("Hello User")
+})
+var server=app.listen(5000,function(){
+console.log("Port started at 5000")
+})
+module.exports=server
