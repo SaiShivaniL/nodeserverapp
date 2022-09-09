9eff2aeef08e3855cec8f19e0d92dfc93309f6af
SaiShivaniL <saishivani.wnp@gmail.com>
2022-09-09 12:54:42 +0530

    index update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express=require('express')
 var app=express()
+const port=process.env.PORT || 3001
 app.get("/",(req,res)=>{
     res.send("Hello User")
 })
-var server=app.listen(5000,function(){
-//console.log("Port started at 5000")
+var server=app.listen(port,function(){
+//console.log("Server started ")
 })
 module.exports=server
